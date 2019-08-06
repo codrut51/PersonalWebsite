c65b19dd22c0751d688961ac0523440e919525a6
Codrut Marin <codrutsoft13@gmail.com>
2019-08-06 16:25:46 +0200

    Redirect to welcome

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
       {
         path:'',
         pathMatch: 'full',
-        redirectTo: "triangles"
+        redirectTo: "welcome"
       },
       {
         path:'welcome',
